975bc0d170f5728081933d4990c8101f10a35781
daniil <korik@DESKTOP-EUOA9KV>
2025-07-15 09:24:14 +0300

    правки

---
+++ WebApplication1/Models/Log/ActivityLog.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models.Log
+{
+    public class ActivityLog
+    {
+        public string ShortDescription { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+        public string CreatedAt { get; set; } = string.Empty;
+    }
+}
